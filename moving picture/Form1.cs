7a2ee8afecd1d71237253b4ef6ffa65217b60f3c
Darcy May Brocklebank <RC0041688@student.riversidecollege.ac.uk>
2020-12-14 15:38:34 +0000

    added left button

--- moving picture/Form1.cs
+++ moving picture/Form1.cs
@@ -21,5 +21,10 @@ namespace moving_picture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
         }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
+        }
     }
 }
